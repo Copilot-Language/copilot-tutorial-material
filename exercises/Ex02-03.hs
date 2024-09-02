081d3ba1eeb483b47df948a3baa4352c95c04cc2
Ivan Perez <ivan.perezdominguez@nasa.gov>
2024-09-02 11:41:20 -0700

    Clarify header

--- exercises/Ex02-03.hs
+++ exercises/Ex02-03.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE RebindableSyntax #-}
 -- Part 2, exercise 3 (10 minutes).
 --
--- We want to check if an aircraft's speed was too low in the previous sample
--- and the current sample (twice in a row).
+-- We want to be robust to small drops in speed that are temporary, so we'll
+-- check if the speed is below the threshold two times in a row. Then it's a
+-- problem.
+--
+-- Therefore, we want to check if an aircraft's speed was too low in the
+-- previous sample and the current sample (twice in a row).
 -- 
 -- How do we do it?
 
